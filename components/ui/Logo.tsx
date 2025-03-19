bad6f18d197ca3f4f34735148b7859b6cb83edb3
Ivan Molto <ivanmolto@gmail.com>
2025-03-19 12:31:49 +0100

    feat: Add Logo component with SVG representation for branding

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const Logo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 768 768"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <defs />
+    <g id="Layer-1">
+      <path
+        d="M325.631 772.184L325.631 723.444L471.111 296.314L539.871 296.314L685.451 721.354L685.821 772.184L325.631 772.184Z"
+        fill="#dc2626"
+        fillRule="nonzero"
+        opacity="1"
+        stroke="none"
+      />
+      <path
+        d="M224.701 888.074L224.701 811.274L456.751 130.174L563.711 130.174L796.751 811.244L796.751 888.074L224.701 888.074Z"
+        fill="#dc2626"
+        fillRule="nonzero"
+        opacity="1"
+        stroke="none"
+      />
+      <path
+        d="M325.631 772.184L325.631 723.444L471.111 296.314L539.871 296.314L685.451 721.354L685.821 772.184L325.631 772.184Z"
+        fill="#ef4444"
+        fillRule="nonzero"
+        opacity="1"
+        stroke="none"
+      />
+      <path
+        d="M423.711 673.224L423.711 647.434L488.791 455.464L521.981 454.914L587.511 646.784L587.511 673.224L423.711 673.224Z"
+        fill="#f87171"
+        fillRule="nonzero"
+        opacity="1"
+        stroke="none"
+      />
+    </g>
+  </svg>
+);
+export { Logo };
