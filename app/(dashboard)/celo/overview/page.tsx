4f1a4b9859465bebe68a0d8aa6382946ec1cedfc
Ivan Molto <ivanmolto@gmail.com>
2025-03-29 15:26:48 +0100

    feat: Add Celo Overview page component for cumulative on-chain attestations visualization using a line chart

---
+++ app/(dashboard)/celo/overview/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { LineChart } from "@/components/LineChart";
+import { chartLineData } from "@/data/data";
+import { cx } from "@/lib/utils";
+
+export default function Overview() {
+  return (
+    <section aria-labelledby="usage-overview">
+      <dl
+        className={cx(
+          "ml-2 mt-10 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
+        )}
+      >
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Cumulative On-Chain Attestations over time
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Total number of attestations accumulated over time
+            </dd>
+          </div>
+          <LineChart
+            className="h-60 w-full"
+            colors={["yellow"]}
+            data={chartLineData}
+            index="date"
+            categories={["Cumulative Attestations"]}
+            valueFormatter={(number: number) =>
+              `$${Intl.NumberFormat("us").format(number).toString()}`
+            }
+            onValueChange={(v) => console.log(v)}
+          />
+        </div>
+      </dl>
+    </section>
+  );
+}
