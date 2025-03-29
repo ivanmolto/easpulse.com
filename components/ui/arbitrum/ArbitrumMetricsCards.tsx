63bcd20b74c7ebef523b84f35a07e90c5826707f
Ivan Molto <ivanmolto@gmail.com>
2025-03-29 10:59:40 +0100

    feat: Add Arbitrum One metrics cards component to display key metrics for on-chain attestations

---
+++ components/ui/arbitrum/ArbitrumMetricsCards.tsx
@@ -0,0 +1,27 @@
+export function ArbitrumMetricsCards() {
+  return (
+    <>
+      <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-50">
+        Overview
+      </h1>
+      <dl className="mt-6 flex flex-wrap items-center gap-x-12 gap-y-8">
+        <div>
+          <dt className="text-sm text-gray-500 dark:text-gray-500">
+            On-Chain Attestations
+          </dt>
+          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
+            {new Intl.NumberFormat("en-US").format(94521)}
+          </dd>
+        </div>
+        <div>
+          <dt className="text-sm text-gray-500 dark:text-gray-500">
+            Unique On-Chain Recipients
+          </dt>
+          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
+            {new Intl.NumberFormat("en-US").format(42405)}
+          </dd>
+        </div>
+      </dl>
+    </>
+  );
+}
