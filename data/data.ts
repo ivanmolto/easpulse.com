1635e2a1789ffd602f52e4abf3e96816e5cc4884
Ivan Molto <ivanmolto@gmail.com>
2025-03-31 14:25:46 +0200

    feat: Add data chart and line data for on-chain attestations and metrics visualization

---
+++ data/data.ts
@@ -0,0 +1,203 @@
+interface DataChart {
+  "# Attestations": number;
+  Day: string;
+}
+
+export const dataChart: DataChart[] = [
+  {
+    "# Attestations": 95366,
+    Day: "2024-10-01 00:00:00.000 UTC",
+  },
+  {
+    "# Attestations": 177700,
+    Day: "2024-07-01 00:00:00.000 UTC",
+  },
+  {
+    "# Attestations": 41803,
+    Day: "2023-10-01 00:00:00.000 UTC",
+  },
+  {
+    "# Attestations": 47751,
+    Day: "2025-01-01 00:00:00.000 UTC",
+  },
+  {
+    "# Attestations": 20610,
+    Day: "2023-07-01 00:00:00.000 UTC",
+  },
+  {
+    "# Attestations": 122404,
+    Day: "2024-04-01 00:00:00.000 UTC",
+  },
+  {
+    "# Attestations": 94562,
+    Day: "2024-01-01 00:00:00.000 UTC",
+  },
+];
+
+export const chartLineData = [
+  {
+    day: "Jan 1",
+    "Cumulative Attestations": 2890,
+    Inverters: 2338,
+  },
+  {
+    day: "Jan 2",
+    "Cumulative Attestations": 2756,
+    Inverters: 2103,
+  },
+  {
+    day: "Jan 3",
+    "Cumulative Attestations": 3322,
+    Inverters: 2194,
+  },
+  {
+    day: "Jan 4",
+    "Cumulative Attestations": 3470,
+    Inverters: 2108,
+  },
+  {
+    day: "Jan 5",
+    "Cumulative Attestations": 3475,
+    Inverters: 1812,
+  },
+  {
+    day: "Jan 6",
+    "Cumulative Attestations": 3129,
+    Inverters: 1726,
+  },
+  {
+    day: "Jan 7",
+    "Cumulative Attestations": 3490,
+    Inverters: 1982,
+  },
+  {
+    day: "Jan 8",
+    "Cumulative Attestations": 2903,
+    Inverters: 2012,
+  },
+  {
+    day: "Jan 9",
+    "Cumulative Attestations": 2643,
+    Inverters: 2342,
+  },
+  {
+    day: "Jan 10",
+    "Cumulative Attestations": 2837,
+    Inverters: 2473,
+  },
+  {
+    day: "Jan 11",
+    "Cumulative Attestations": 2954,
+    Inverters: 3848,
+  },
+  {
+    day: "Jan 12",
+    "Cumulative Attestations": 3239,
+    Inverters: 3736,
+  },
+  {
+    day: "Jan 13",
+    "Cumulative Attestations": 2890,
+    Inverters: 2338,
+  },
+  {
+    day: "Jan 14",
+    "Cumulative Attestations": 2756,
+    Inverters: 2103,
+  },
+  {
+    day: "Jan 15",
+    "Cumulative Attestations": 3322,
+    Inverters: 2194,
+  },
+  {
+    day: "Jan 16",
+    "Cumulative Attestations": 3470,
+    Inverters: 2108,
+  },
+  {
+    day: "Jan 17",
+    "Cumulative Attestations": 3475,
+    Inverters: 1812,
+  },
+  {
+    day: "Jan 18",
+    "Cumulative Attestations": 3129,
+    Inverters: 1726,
+  },
+  {
+    day: "Jan 19",
+    "Cumulative Attestations": 3490,
+    Inverters: 1982,
+  },
+  {
+    day: "Jan 20",
+    "Cumulative Attestations": 2903,
+    Inverters: 2012,
+  },
+  {
+    day: "Jan 21",
+    "Cumulative Attestations": 2643,
+    Inverters: 2342,
+  },
+  {
+    day: "Jan 22",
+    "Cumulative Attestations": 2837,
+    Inverters: 2473,
+  },
+  {
+    day: "Jan 23",
+    "Cumulative Attestations": 2954,
+    Inverters: 3848,
+  },
+  {
+    day: "Jan 24",
+    "Cumulative Attestations": 3239,
+    Inverters: 3736,
+  },
+  {
+    day: "Jan 25",
+    "Cumulative Attestations": 2890,
+    Inverters: 2338,
+  },
+  {
+    day: "Jan 26",
+    "Cumulative Attestations": 2756,
+    Inverters: 2103,
+  },
+  {
+    day: "Jan 27",
+    "Cumulative Attestations": 3322,
+    Inverters: 2194,
+  },
+  {
+    day: "Jan 28 ",
+    "Cumulative Attestations": 3470,
+    Inverters: 2108,
+  },
+  {
+    day: "Jan 29",
+    "Cumulative Attestations": 3475,
+    Inverters: 1812,
+  },
+  {
+    day: "Jan 30",
+    "Cumulative Attestations": 3129,
+    Inverters: 1726,
+  },
+  {
+    day: "Jan 31",
+    "Cumulative Attestations": 3490,
+    Inverters: 1982,
+  },
+  {
+    day: "Feb 1",
+    "Cumulative Attestations": 2903,
+    Inverters: 2012,
+  },
+  {
+    day: "Feb 2",
+    "Cumulative Attestations": 2643,
+    Inverters: 2342,
+  },
+];
