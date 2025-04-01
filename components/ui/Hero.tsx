39fd8cee53aa8449e0f1e08f3a5897ff4fd7439d
Ivan Molto <ivanmolto@gmail.com>
2025-04-01 18:03:44 +0200

    fix: Update links in Layout and Hero components to point to the new Base overview path

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -41,7 +41,7 @@ export function Hero() {
         <FadeDiv>
           <Link
             className="mt-6 inline-flex cursor-pointer flex-row items-center justify-center gap-1 rounded-md border-b-[1.5px] border-red-700 bg-linear-to-b from-red-400 to-red-500 px-5 py-3 leading-4 font-medium tracking-wide whitespace-nowrap text-white shadow-[0_0_0_2px_rgba(0,0,0,0.04),0_0_14px_0_rgba(255,255,255,0.19)] transition-all duration-200 ease-in-out hover:shadow-red-300"
-            href="/passport/overview"
+            href="/base/overview"
           >
             Unleash Now
           </Link>
