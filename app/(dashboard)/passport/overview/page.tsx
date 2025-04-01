fc738cb988618646759d7f3f4836dff59d3b562a
Ivan Molto <ivanmolto@gmail.com>
2025-04-01 20:31:57 +0200

    feat: Add cumulative attestations component to Passport overview, enhancing data visualization and providing detailed insights on on-chain attestations

--- app/(dashboard)/passport/overview/page.tsx
+++ app/(dashboard)/passport/overview/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CumulativeAttestations from "@/components/ui/passport/Cumulative Attestations";
 import { cx } from "@/lib/utils";
 
 export default function Overview() {
@@ -9,7 +10,19 @@ export default function Overview() {
         className={cx(
           "mt-7 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
         )}
-      ></dl>
+      >
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Cumulative On-Chain Attestations over time
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Total number of attestations accumulated over time
+            </dd>
+          </div>
+          <CumulativeAttestations slug="cumulative-arbitrum-eas-onchain-attestations" />
+        </div>
+      </dl>
     </section>
   );
 }
