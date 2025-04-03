dc2ec1d0509337d394827ff1ef992602f1078768
Ivan Molto <ivanmolto@gmail.com>
2025-04-03 23:46:05 +0200

    feat: Add attestations count, recipients count, and revenue components to Gitcoin metrics cards for enhanced data visualization and user insights

--- components/ui/gitcoin/GitcoinMetricsCards.tsx
+++ components/ui/gitcoin/GitcoinMetricsCards.tsx
@@ -1,3 +1,7 @@
+import AttestationsCount from "@/components/ui/gitcoin/AttestationsCount";
+import RecipientsCount from "@/components/ui/gitcoin/RecipientsCount";
+import Revenue from "@/components/ui/gitcoin/Revenue";
+
 export function GitcoinMetricsCards() {
   return (
     <>
@@ -9,25 +13,19 @@ export function GitcoinMetricsCards() {
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             On-Chain Attestations
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(18971)}
-          </dd>
+          <AttestationsCount slug="gitcoin-onchain-attestations" />
         </div>
         <div>
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             Unique On-Chain Recipients
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(16394)}
-          </dd>
+          <RecipientsCount slug="gitcoin-recipients-count" />
         </div>
         <div>
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             On-Chain Attestations Revenue
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(14.62)} ETH
-          </dd>
+          <Revenue slug="gitcoin-attestations-revenue" />
         </div>
       </dl>
     </>
