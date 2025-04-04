b3f7355d46a646cd4759e40e4880a9d5363d5a43
Ivan Molto <ivanmolto@gmail.com>
2025-04-04 10:36:05 +0200

    feat: Add attestations count and recipients count components to Base metrics cards for improved data visualization

--- components/ui/base/BaseMetricsCards.tsx
+++ components/ui/base/BaseMetricsCards.tsx
@@ -1,3 +1,6 @@
+import AttestationsCount from "@/components/ui/base/AttestationsCount";
+import RecipientsCount from "@/components/ui/base/RecipientsCount";
+
 export function BaseMetricsCards() {
   return (
     <>
@@ -9,17 +12,13 @@ export function BaseMetricsCards() {
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             On-Chain Attestations
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(1064225)}
-          </dd>
+          <AttestationsCount slug="base-attestations-count" />
         </div>
         <div>
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             Unique On-Chain Recipients
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(650119)}
-          </dd>
+          <RecipientsCount slug="base-recipients-count" />
         </div>
       </dl>
     </>
