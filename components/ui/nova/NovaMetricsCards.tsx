587caaff79c3cd38f86244f28e2e2a450de61cf0
Ivan Molto <ivanmolto@gmail.com>
2025-04-05 20:23:46 +0200

    feat: Add attestations count and recipients count components for Nova metrics cards to enhance data visualization

--- components/ui/nova/NovaMetricsCards.tsx
+++ components/ui/nova/NovaMetricsCards.tsx
@@ -1,3 +1,6 @@
+import AttestationsCount from "@/components/ui/nova/AttestationsCount";
+import RecipientsCount from "@/components/ui/nova/RecipientsCount";
+
 export function NovaMetricsCards() {
   return (
     <>
@@ -9,17 +12,13 @@ export function NovaMetricsCards() {
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             On-Chain Attestations
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(94521)}
-          </dd>
+          <AttestationsCount slug="nova-eas-total-onchain-attestations-count" />
         </div>
         <div>
           <dt className="text-sm text-gray-500 dark:text-gray-500">
             Unique On-Chain Recipients
           </dt>
-          <dd className="text-lg font-semibold text-gray-900 dark:text-gray-50 mt-1.5 flex items-center gap-2">
-            {new Intl.NumberFormat("en-US").format(42405)}
-          </dd>
+          <RecipientsCount slug="nova-total-eas-unique-onchain-recipients" />
         </div>
       </dl>
     </>
