a36b48e6a2891ec2027affa9af902e70599737f7
Ivan Molto <ivanmolto@gmail.com>
2025-04-06 21:06:03 +0200

    fix: Change chart color from yellow to amber in Scroll cumulative attestations component for improved visual consistency

--- components/ui/scroll/CumulativeAttestations.tsx
+++ components/ui/scroll/CumulativeAttestations.tsx
@@ -64,7 +64,7 @@ export default function CumulativeAttestations({
     <>
       <LineChart
         className="h-60 w-full"
-        colors={["yellow"]}
+        colors={["amber"]}
         data={sortedDataChart}
         index="Day"
         categories={["# Attestations"]}
